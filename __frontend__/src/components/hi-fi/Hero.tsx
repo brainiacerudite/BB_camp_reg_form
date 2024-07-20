2a76831ba32a4c3f68cd022a9ba7908defd9e727
Brainiac Erudite <brainiachades@gmail.com>
2024-07-20 19:11:29 +0100

    Updated the frontend for desktop to match UI design

--- __frontend__/src/components/hi-fi/Hero.tsx
+++ __frontend__/src/components/hi-fi/Hero.tsx
@@ -10,7 +10,7 @@ function Hero() {
       className="w-full min-h-[100svh] relative bg-cover bg-center flex justify-center items-center px-4"
     >
       <div className="w-full flex flex-col space-y-16">
-        <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col justify-center items-center gap-4 lg:hidden">
           <Anchor />
           <h4 className="text-2xl text-center text-white font-bold">
             2024 ACB <br />
@@ -18,10 +18,26 @@ function Hero() {
           </h4>
         </div>
 
-        <div className="px-4 py-10 border-gradient rounded-2xl bg-bgGlassMorphism backdrop-blur-sm">
-          <div className="w-full max-w-[360px] p-4 flex flex-col gap-4">
-            <LinkSolidButton to="/" text="REGISTER NOW" type="button" />
-            <LinkOutlineButton to="/" text="CHECK REG STATUS" type="button" />
+        <div className="lg:inline-block lg:mx-auto">
+          <div className="px-4 py-10 border-gradient rounded-2xl bg-bgGlassMorphism backdrop-blur-sm lg:p-12">
+            <div className="lg:flex lg:items-center lg:space-x-12">
+              <div className="hidden w-full flex-col justify-center items-center gap-4 lg:flex lg:w-[488px]">
+                <Anchor />
+                <h4 className="text-2xl text-center text-white font-bold">
+                  2024 ACB <br />
+                  CAMP EXERCISE
+                </h4>
+              </div>
+
+              <div className="w-full max-w-[360px] p-4 flex flex-col justify-center space-y-4 lg:w-[488px] lg:px-8 lg:py-8 lg:space-y-8 lg:h-96">
+                <LinkSolidButton to="/" text="REGISTER NOW" type="button" />
+                <LinkOutlineButton
+                  to="/"
+                  text="CHECK REG STATUS"
+                  type="button"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
