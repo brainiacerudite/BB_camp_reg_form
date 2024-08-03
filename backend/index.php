22f502c1b36d6d39cb983e58515c139e283d3ba9
Oladeji Oluwasegun Thomas <brainiachades@gmail.com>
2024-08-03 08:58:41 +0100

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -23,8 +23,10 @@ $router = new Router();
 // Define routes
 $router->post('/register', ['App\Controllers\RegisterController', 'register']);
 $router->get('/check', ['App\Controllers\RegisterController', 'check']);
+$router->get('/tag', ['App\Controllers\RegisterController', 'tag']);
 // Define panel routes
 $router->get('/panel/list', ['App\Controllers\PanelController', 'list']);
+$router->get('/panel/search', [App\Controllers\PanelController', 'search']);
 
 // error_log("Actual Request URI: " . $_SERVER['REQUEST_URI']);
 // exclude query params from $_SERVER['REQUEST_URI']
